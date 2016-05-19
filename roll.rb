b1275a7d76b2d62cfa3b971bb9844a70f4446517
Nicholas R. Holmstedt <holmstn@gmail.com>
2016-05-18 22:25:29 -0400

    Dice Roller, first try

---
+++ roll.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require 'httparty'
+require 'json'
+
+InvalidTokenError = Class.new(Exception)
+
+def display_message(message)
+  content_type :json
+  {:text => message}.to_json
+end
+
+def roll(modifier)
+  die = [-1, 0, 1]
+
+  rolls = []
+  4.times { rolls << die.sample }
+
+  roll_value = rolls.inject(:+) + modifier
+end
+
+
+post '/' do
+  raise(InvalidTokenError) unless params[:token] == ENV['SLACK_TOKEN']
+
+  text = params[:text].strip
+
+  if text =~ /[+-]\d+/
+    total = roll text.to_i
+    display_message "You rolled a #{total}"
+  else
+    display_message "Must follow format: '\/roll \<num\>'"
+  end
+end
